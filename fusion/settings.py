51d5d8483643bc0a1824cb5a1ad938200fd44491
James Mudidi <mudidi.jimmy@gmail.com>
2019-12-01 17:00:35 +0300

    setup database

--- fusion/settings.py
+++ fusion/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fusion',
-    'fusion.apps.welcome',
+    'welcome',
     'rest_framework',
 ]
 
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "fusion",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
--- fusion/urls.py
+++ fusion/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('fusion.apps.welcome.urls')),
+    path('api/', include('welcome.urls')),
 ]
