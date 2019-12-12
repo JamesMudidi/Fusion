b4a0c810ac996e0f5b9c250818d2314c6dca0abe
James Mudidi <mudidi.jimmy@gmail.com>
2019-12-13 02:00:43 +0300

    make it better

--- fusion/settings.py
+++ fusion/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
